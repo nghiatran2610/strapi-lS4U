97fba0327a1f456673e064d88523c70aeedaa45e
nghiatran2610 <73481028+nghiatran2610@users.noreply.github.com>
2024-07-28 14:10:34 +0700

    edit cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,31 +1,25 @@
 module.exports = [
-  "strapi::errors",
+  'strapi::errors',
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: ["http://localhost:3000", "https://vietspecials.com"], // Add your development and production URLs here
-    },
-  },
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
