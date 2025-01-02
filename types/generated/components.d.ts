a68927b599e1b143691f46815b22a23d0a7777a2
nghiatran2610 <73481028+nghiatran2610@users.noreply.github.com>
2025-01-03 02:03:20 +0700

    added dynamic component

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface MediacomponentsVideoEmbed extends Schema.Component {
+  collectionName: 'components_mediacomponents_video_embeds';
+  info: {
+    displayName: 'VideoEmbed';
+    icon: 'medium';
+    description: '';
+  };
+  attributes: {
+    Width: Attribute.Float;
+    Height: Attribute.Float;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'mediacomponents.video-embed': MediacomponentsVideoEmbed;
+    }
+  }
 }
